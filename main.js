4289453c3f1487428f1968858233412f3385ddb0
pilseong kang <kangps7675@gmail.com>
2026-02-26 00:50:52 +0900

    second commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const API_KEY = 'b62c99bab821449d96d2bb258fbb183c';
+let newsList = [];
+const menus = document.querySelectorAll('.menus button');
+menus.forEach(menu=> menu.addEventListener('click', (event) =>getNewsByCategory(event)));
 
-let news = [];
 
 const getLatestNews = async () => {
     const url = new URL(
@@ -8,8 +10,72 @@ const getLatestNews = async () => {
     );
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("dddd", news);
+    newsList = data.articles;
+    render();
+    console.log("dddd", newsList);
 };
 
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase();
+    console.log("category", category);
+    const url = new URL(
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+    );
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("data", data);
+    newsList = data.articles;
+    render();
+}
+
+const getNewsByKeyword= async ()=>{
+    const keyword = document.getElementById("search-input").value;
+    console.log("keyword", keyword);
+    const url = new URL
+       (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`);
+    const response = await fetch(url); 
+    const data = await response.json();
+    console.log("data", data);
+    newsList = data.articles;
+    render();
+
+    }
+
+
+document.getElementById("search-input").addEventListener("keydown", (event) => {
+    
+    if (event.key === "Enter") {
+
+        getNewsByKeyword();
+    }
+});
+
+const render = () => {
+    const newsHTML = newsList.map((news) => {
+        // 1. 이미지가 null일 때 사용할 백업 주소
+        const imgUrl = news.urlToImage || "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+
+        return `<div class="row news">
+            <div class="col-lg-4">
+                <img class="news-image-size" 
+                     src="${imgUrl}" 
+                     onerror="this.src='https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg';" 
+                     alt="News Image"/>
+            </div>
+            <div class="col-lg-8">
+                <h2>${news.title}</h2>
+                <p>${news.description || "내용 없음"}</p>
+                <div>
+                    ${news.source.name || "Unknown"} ${news.publishedAt}
+                </div>
+            </div>
+        </div>`;
+    }).join('');
+
+    document.getElementById('news-board').innerHTML = newsHTML;
+};
 getLatestNews();
+
+//1. 버튼들에 클릭 이벤트를 줘야 한다
+//2. 카테고리별 뉴스 가져오기
+//3. 그 뉴스를 보여주기
